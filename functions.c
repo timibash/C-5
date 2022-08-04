c0db3d678b98aa4facadb28e60e1a856e4581336
timibash <85651144+timibash@users.noreply.github.com>
2022-08-04 21:10:52 +0100

    Add files via upload

---
+++ functions.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+int sumtwo(int a, int b,int c,int d) {
+
+    int sum = a+b+c+d;
+    return sum;
+}
+int main() {
+
+    printf("sum of a,b,c and d is %d", sumtwo(1,2,3,4));
+    return 0;
+}
+
+
+
+
+
+
+
+
+
